db51f490bb3117a2fc10b6da9ae31e2ea7e2031f
Patrick Lang <patrick.lang@hotmail.com>
2019-12-07 14:55:23 -0800

    Skip chocolatey install on subsequent provisions

--- Vagrant/install-chocolatey.ps1
+++ Vagrant/install-chocolatey.ps1
@@ -1,1 +1,3 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+if ((Get-Command choco.exe -ErrorAction Ignore) -eq $null) {
+    Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
